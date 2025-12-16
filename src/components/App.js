3738ab396a206264065b4feb300c79e793ed29ff
Shiv <shivshakti.ss771@gmail.com>
2025-12-16 16:04:39 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,37 @@
-
-import React from "react";
-import './../styles/App.css';
+ 
+ import React, { useState, useEffect } from "react";
+import "./../styles/App.css";
 
 const App = () => {
-  return (
-    <div>
+  const [per, setPer] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setPer(prev => {
+        if (prev >= 100) {
+          clearInterval(interval);
+          return 100;
+        }
+        return prev + 10;
+      });
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+
+ return (
+    <div class="parent">
         {/* Do not remove the main div */}
+        <div id="barInner"></div>
+        <div id="barOuter" style={{width:`${per}%`}}></div>
+
     </div>
   )
-}
+};
 
-export default App
+export default App;
+
+ 
+ 
+
