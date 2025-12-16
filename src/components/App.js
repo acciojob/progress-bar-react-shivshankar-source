d7c255511e4abc982724e661133314e35a32abd3
Shiv <shivshakti.ss771@gmail.com>
2025-12-16 19:17:57 +0530

    another commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,38 @@
- 
- import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./../styles/App.css";
 
 const App = () => {
   const [per, setPer] = useState(0);
 
-  useEffect(() => { 
+  useEffect(() => {
     const interval = setInterval(() => {
-      
       setPer(prev => {
         if (prev >= 100) {
           clearInterval(interval);
           return 100;
         }
-        return (prev) + 10;
+        return prev + 10;
       });
     }, 1000);
 
     return () => clearInterval(interval);
   }, []);
 
+  return (
+    <div className="parent">
 
- return (
-    <div class="parent">
-        {/* Do not remove the main div */}
-        <p id="barInner"></p>
-        <p id="barOuter" style={{width:`${per}%`}}></p>
+      <p>{per}%</p>
 
+    
+      <div id="barOuter">
+     
+        <div
+          id="barInner"
+          style={{ width: `${(per / 100) * 200}px` }}
+        ></div>
+      </div>
     </div>
-  )
+  );
 };
 
 export default App;
-
- 
- 
-
